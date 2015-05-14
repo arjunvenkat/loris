9184be7ccc85c43dcf0c0aa3418b489e3d733ac4
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-14 10:20:39 -0500

    adds placeholders for unfinished links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/classes' => 'pages#classes'
   get '/classes/html' => 'pages#html'
+  get '/html' => 'pages#html'
+  get '/structure' => 'pages#structure'
+  get '/pricing' => 'pages#pricing'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
