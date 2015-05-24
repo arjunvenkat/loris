687e8cf8cd7f931b1956785aec6e3242eaf3bc94
Arjun Venkataswamy <avenkat2@gmail.com>
2015-05-24 09:24:55 -0700

    stubs out apply form

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/our_program' => 'pages#our_program', as: 'our_program'
   get '/pricing' => 'pages#pricing'
   get '/about' => 'pages#about'
+  get '/apply' => 'pages#apply'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
