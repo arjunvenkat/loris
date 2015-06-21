9cc0e618580691946f5946c17e8c9551375e74c1
Arjun Venkataswamy <avenkat2@gmail.com>
2015-06-21 00:08:02 -0500

    adds associations for course section and track

---
+++ app/admin/course.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register Course do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :name,
+                :track_id,
+                :image,
+                :level,
+                :short_desc,
+                :long_desc,
+                :end_goal_desc,
+                :end_goal_image,
+                :end_goal_link,
+                :timeline
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ActiveRecord::Base
+  has_many :sections
+  belongs_to :track
 end
